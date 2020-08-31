c43702dffc5482d7d570537873340d36d9f94433
omenlog <omar.enrique1211@gmail.com>
2020-08-31 22:34:18 +0300

    chore: fix lint warnings , update lint rules

--- src/runners.ts
+++ src/runners.ts
@@ -1,37 +1,38 @@
 import { EventIterator, Event_ } from './types';
 
 const validateOperation = (ev: Event_) => {
-  if(!ev.meta || !ev.fn){
+  if (!ev.meta || !ev.fn) {
     throw new Error('Invalid Operation');
   }
-} 
+};
 
-async function run(it: EventIterator, payload: any = {}): Promise<any> | never  {
+async function run(it: EventIterator, payload: any = {}): Promise<any> | never {
   let iter = it.next(payload);
   let result;
 
-  while(!iter.done) {
-    validateOperation(iter.value)
+  while (!iter.done) {
+    validateOperation(iter.value);
     result = await iter.value.fn(it);
-    iter = it.next(result)
+    iter = it.next(result);
   }
- 
-  return iter.value
+
+  return iter.value;
 }
 
 async function testRun(
   it: EventIterator,
   payload: any,
   events: any
-): Promise<{result: any, events: any}> {
+): Promise<{ result: any; events: any }> {
   let iter = it.next(payload);
   let result;
 
-  while(!iter.done){
-    const {value} = iter;
-    validateOperation(value)
+  while (!iter.done) {
+    const { value } = iter;
+    validateOperation(value);
 
-    if(value.meta.type === 'event-emitted'){
+    if (value.meta.type === 'event-emitted') {
+      // eslint-disable-next-line no-param-reassign
       events[value.meta.name] = { args: value.meta.args };
     }
 
@@ -39,7 +40,7 @@ async function testRun(
     iter = it.next(result);
   }
 
-  return {result : iter.value, events}
+  return { result: iter.value, events };
 }
 
 export { run, testRun };
