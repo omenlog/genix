6cfd2213c5a18439c7dab803b1e2cd063924686a
omenlog <omar.enrique1211@gmail.com>
2020-09-25 16:57:55 +0300

    ref: change lib api removing function used by generators - GH-35

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,10 @@
-import { command, onCommand } from './commands';
-import { emit, onEvent } from './events';
-import { register, exec, g, mapEvents } from './sources';
-import testTools from './test-tools';
-import initSrc from './init-src';
+import { exec, onCommand } from './commands';
+import { emit, onEvent, mapEvents } from './events';
+import { wrap } from './test-tools';
 
-exec(initSrc);
-
-export {
-  exec,
-  command,
-  emit,
-  register,
-  g,
-  mapEvents,
-  onEvent,
-  onCommand,
-  testTools,
+const genix = {
+  wrap,
 };
+
+export default genix;
+export { exec, emit, mapEvents, onEvent, onCommand };
